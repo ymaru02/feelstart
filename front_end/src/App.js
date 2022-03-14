a2b3a0445a83a219195c300e38d02f3f72f69ce7
eora21 <eora21@naver.com>
2022-03-14 23:38:10 +0900

    REFACTOR: appjs

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import Map from "./components/Atoms/Map";
 import { Route, Routes } from "react-router-dom";
 import About from "./components/Pages/About";
 import Home from "./components/Pages/Home";
@@ -7,7 +6,6 @@ import Home from "./components/Pages/Home";
 const App = () => {
   return (
     <div className="App">
-      {/* <Map /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
