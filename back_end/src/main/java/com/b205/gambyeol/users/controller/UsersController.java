67cd0b176dd6e40940f1565734b4a5e56a6cbbdf
배나영 <qoskdud15@daum.net>
2022-03-16 15:48:01 +0900

    chore: 백엔드 스켈레톤 코드 작성

---
+++ back_end/src/main/java/com/b205/gambyeol/users/controller/UsersController.java
@@ -0,0 +1,4 @@
+package com.b205.gambyeol.users.controller;
+
+public class UsersController {
+}
--- back_end/src/main/java/com/b205/gambyeol/users/domain/UsersRepository.java
+++ back_end/src/main/java/com/b205/gambyeol/users/domain/UsersRepository.java
@@ -2,6 +2,6 @@ package com.b205.gambyeol.users.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsersRepository extends JpaRepository<Users, String> {
+public interface UsersRepository extends JpaRepository<Users, Long> {
 
 }
