221968124ed0b21c7207280301ea148c680efc2e
eora21 <eora21@naver.com>
2022-03-17 15:41:02 +0900

    FEAT: kakao axios

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -12,7 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        {/* <Route path="/kakaocallback" element={<KakaoCallback />} /> */}
+        <Route path="/kakaocallback" element={<KakaoCallback />} />
       </Routes>
     </div>
   );
