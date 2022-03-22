bfbb365a6a7de0dac2606cb09edcbec2a9a4c257
eora21 <eora21@naver.com>
2022-03-22 23:46:02 +0900

    REFACTOR: make onbording

--- front_end/src/components/Atoms/Title.jsx
+++ front_end/src/components/Atoms/Title.jsx
@@ -1,21 +1,28 @@
 import { AppBar, Typography } from "@mui/material";
 import styles from "styles.module.css";
+import Box from "@mui/material/Box";
 
 const Title = () => {
   return (
-    <AppBar position="sticky" id={styles.indigo} style={{ Height: "10vh" }}>
-      <Typography
-        variant="h5"
-        align="left"
-        className={styles.apricot}
-        style={{ margin: "5px" }}
-      >
-        <span className={styles.orange}>감</span>
-        <span>정은</span>
-        <br />
-        <span className={styles.orange}>별</span>
-        <span>이 되어</span>
-      </Typography>
+    <AppBar
+      position="sticky"
+      id={styles.indigo}
+      style={{ height: "74px", alignItems: "center" }}
+    >
+      <Box maxWidth="1000px" width="100%">
+        <Typography
+          variant="h5"
+          align="left"
+          className={styles.apricot}
+          style={{ margin: "5px" }}
+        >
+          <span className={styles.orange}>감</span>
+          <span>정은</span>
+          <br />
+          <span className={styles.orange}>별</span>
+          <span>이 되어</span>
+        </Typography>
+      </Box>
     </AppBar>
   );
 };
--- front_end/src/components/Pages/Home.jsx
+++ front_end/src/components/Pages/Home.jsx
@@ -1,21 +1,7 @@
-import KakaoLoginButton from "components/Atoms/KakaoLoginButton";
-// import Map from "components/Atoms/Map";
-import Box from "@mui/material/Box";
-import styles from "styles.module.css";
-import Carousel from "components/Organisms/Carousel";
+import Onbording from "components/Templates/Onbording";
 
 const Home = () => {
-  return (
-    <Box display="flex" flexDirection="column" justifyContent="space-between">
-      {/* <Map /> */}
-      <Box flexGrow="1">
-        <Carousel />
-      </Box>
-      <Box id={styles.indigo} sx={{ mt: 3 }}>
-        <KakaoLoginButton />
-      </Box>
-    </Box>
-  );
+  return <Onbording />;
 };
 
 export default Home;
