9e5637c1c870449ec13e57f63bf5ecb9fcf00485
Ymaru02 <dyun022@gmail.com>
2022-03-29 23:52:35 +0900

    modify and feat content

---
+++ front_end/src/components/Atoms/ListSet.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+
+export default function BasicList() {
+  return (
+    <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
+      <nav aria-label="main mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText
+                primaryTypographyProps={{ color: "red", textAlign: "center" }}
+                primary="신고"
+              />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText
+                primaryTypographyProps={{ textAlign: "center" }}
+                primary="팔로우"
+              />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+      <Divider />
+      <nav aria-label="secondary mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText
+                primaryTypographyProps={{ textAlign: "center" }}
+                primary="게시물로 이동.."
+              />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemText
+                primaryTypographyProps={{ textAlign: "center" }}
+                primary="취소"
+              />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+    </Box>
+  );
+}
--- front_end/src/components/Molecules/RecipeReviewCard.jsx
+++ front_end/src/components/Molecules/RecipeReviewCard.jsx
@@ -1,4 +1,7 @@
-import * as React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
@@ -7,51 +10,167 @@ import CardActions from "@mui/material/CardActions";
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
+import CommentIcon from "@mui/icons-material/Comment";
+import MapIcon from "@mui/icons-material/Map";
+import Collapse from "@mui/material/Collapse";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+import Map from "components/Atoms/Map";
+import AvatarCircle from "components/Atoms/AvatarCircle";
+import ModalSet from "components/Atoms/ModalSet";
+
+import styles from "styles.module.css";
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 export default function RecipeReviewCard({ imgurl }) {
+  const [expanded, setExpanded] = useState(false);
+  const [favor, setFavor] = useState(false);
+  const [openmap, setOpenMap] = useState(false);
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+
+  const handleExpandClick = () => setExpanded(!expanded);
+  const handleFavorClick = () => setFavor((current) => !current);
+  const handleClickMap = () => setOpenMap((cur) => !cur);
+  const handelSubmit = (event) => {
+    event.preventDefault();
+    if (comment === "") return;
+    setComments((currentArray) => [...currentArray, comment]);
+    setComment("");
+    console.log(comments);
+  };
+  const handelChange = (event) => {
+    setComment(event.target.value);
+  };
   return (
-    <Card sx={{ maxWidth: 1000 }}>
+    <Card
+      sx={{ maxWidth: 1000, border: 1, borderColor: "#c0c0c0" }}
+      style={{
+        backgroundColor: "#fff",
+      }}
+    >
       <CardHeader
         avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
+          <>
+            <AvatarCircle />
+            <Typography style={{ margin: "auto 5px" }}>user.name</Typography>
+          </>
         }
         action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
+          <>
+            <IconButton
+              variant="outlined"
+              color="inherit"
+              aria-label="map"
+              onClick={handleClickMap}
+            >
+              <MapIcon
+                className={styles.grey}
+                style={{
+                  marginTop: "8px",
+                  textdecoration: "none",
+                }}
+              />
+            </IconButton>
+            <ModalSet />
+          </>
         }
         title="Shrimp and Chorizo Paella"
         subheader="September 14, 2016"
-        style={{ color: "white" }}
-        // sx={{ bgcolor: "text.primary", color: "background.paper" }}
+        subheaderTypographyProps={{ color: "#a0a0a0" }}
+        style={{ color: "#a0a0a0" }}
       />
-      <CardMedia
-        sx={{ maxHeight: 600 }}
-        component="img"
-        image={imgurl}
-        alt="Paella dish"
-      />
+      <Box>
+        {openmap ? (
+          <Map />
+        ) : (
+          <CardMedia
+            sx={{ maxWidth: 1000, maxHeight: 1000 }}
+            component="img"
+            image={imgurl}
+            alt="Paella dish"
+          />
+        )}
+      </Box>
+
       <CardContent>
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="body2">
           This impressive paella is a perfect party dish and a fun meal to cook
           together with your guests. Add 1 cup of frozen peas along with the
           mussels, if you like.
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
+        <IconButton
+          onClick={handleFavorClick}
+          variant="outlined"
+          style={favor ? { color: "#ff3333" } : { color: "#a0a0a0" }}
+          aria-label="add to favorites"
+        >
           <FavoriteIcon />
         </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
+        <IconButton
+          variant="outlined"
+          style={{ color: "#a0a0a0" }}
+          aria-label="share"
+        >
+          <CommentIcon />
         </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+          variant="outlined"
+          style={{ color: "#a0a0a0" }}
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
       </CardActions>
+      <Collapse
+        sx={{ border: 1, borderColor: "#c0c0c0" }}
+        in={expanded}
+        timeout="auto"
+        unmountOnExit
+      >
+        <CardContent>
+          <Box>
+            {comments.map((value, index) => (
+              <Typography paragraph key={index}>
+                {value}
+              </Typography>
+            ))}
+          </Box>
+          <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+            <Avatar sx={{ mr: 1, my: 0.5 }}></Avatar>
+            <form style={{ width: "100%" }} onSubmit={handelSubmit}>
+              <TextField
+                onChange={handelChange}
+                fullWidth
+                id="comment"
+                label="댓글달기"
+                variant="filled"
+                value={comment}
+                // InputProps={{ style: { color: "#c0c0c0" } }}
+                InputLabelProps={{ style: { color: "#c0c0c0" } }}
+              />
+            </form>
+          </Box>
+        </CardContent>
+      </Collapse>
     </Card>
   );
 }
