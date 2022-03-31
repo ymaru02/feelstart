d98daf8c2333180dc3b4bd922041b0449cf7e874
배나영 <qoskdud15@daum.net>
2022-03-31 14:16:58 +0900

    FIX: CORS 처리 주석

--- back_end/src/main/java/com/b205/gambyeol/config/WebMvcConfig.java
+++ back_end/src/main/java/com/b205/gambyeol/config/WebMvcConfig.java
@@ -16,17 +16,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS=3600;
 
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(MAX_AGE_SECS);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true)
+//                .maxAge(MAX_AGE_SECS);
+//    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
