b791fd22f130d4b37230da349cad2e23a0ef63b7
eora21 <eora21@naver.com>
2022-04-04 14:48:18 +0900

    REFACTOR: 백 세팅대로 axios 세팅 변경

--- front_end/src/Store/loginStore.js
+++ front_end/src/Store/loginStore.js
@@ -1,7 +1,8 @@
 import create from "zustand";
 
 export const loginStore = create((set, get) => ({
-  jwtToken: "",
+  jwtToken:
+    "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI0IiwiaXNzIjoiZ2FtYnllb2wiLCJpYXQiOjE2NDkwMzU1MzksImV4cCI6MTY0OTEyMTkzOX0.MrVLtV4qsET5nkyw9ff8FgJ-lttuCsQ-A2Uoe0onNqFRuythavU_UX7p4czfBI4zgQmK7dBRtvmGek3oqx6PWQ",
   userId: "",
   isLogined: () => !!get().jwtToken,
   getJwtToken: () => get().jwtToken,
