35e2629ead685f91900af926b4a98b330252877b
eora21 <eora21@naver.com>
2022-04-04 14:50:30 +0900

    FIX: token 초기화

--- front_end/src/Store/loginStore.js
+++ front_end/src/Store/loginStore.js
@@ -1,8 +1,7 @@
 import create from "zustand";
 
 export const loginStore = create((set, get) => ({
-  jwtToken:
-    "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI0IiwiaXNzIjoiZ2FtYnllb2wiLCJpYXQiOjE2NDkwMzU1MzksImV4cCI6MTY0OTEyMTkzOX0.MrVLtV4qsET5nkyw9ff8FgJ-lttuCsQ-A2Uoe0onNqFRuythavU_UX7p4czfBI4zgQmK7dBRtvmGek3oqx6PWQ",
+  jwtToken: "",
   userId: "",
   isLogined: () => !!get().jwtToken,
   getJwtToken: () => get().jwtToken,
